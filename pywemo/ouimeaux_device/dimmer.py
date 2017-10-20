36f1e4203d586adc702ceac66433da9786eb71f5
angel12 <spencer@angelsolutionsnm.com>
2017-10-20 08:04:12 -0600

    Update dimmer.py

--- pywemo/ouimeaux_device/dimmer.py
+++ pywemo/ouimeaux_device/dimmer.py
@@ -1,37 +1,13 @@
-from . import Device
+from .switch import Switch
 
-
-class Dimmer(Device):
-
-    def set_state(self, state):
-        """
-        Set the state of this device to on or off.
-        """
-        self.basicevent.SetBinaryState(BinaryState=int(state))
-        self._state = int(state)
-
-    def off(self):
-        """
-        Turn this device off. If already off, will return "Error".
-        """
-        return self.set_state(0)
-
-    def on(self):
-        """
-        Turn this device on. If already on, will return "Error".
-        """
-        return self.set_state(1)
-
-    def toggle(self):
-        """
-        Toggle the dimmer's state.
-        """
-        return self.set_state(not self.get_state())
-		
+class Dimmer(Switch):		
     def set_brightness(self, brightness):
         """
         Set the brightness of this device to an integer between 1-100.
         """
+        if self.get_state() == 0 or None:
+            self.on()
+
         self.basicevent.SetBinaryState(brightness=int(brightness))
         self._brightness = int(brightness)
 
