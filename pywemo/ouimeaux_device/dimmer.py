f8b529908cd2bf51990844a43f4b47b1deb72fcc
angel12 <spencer@angelsolutionsnm.com>
2017-10-20 13:03:10 -0600

    Added get_brightness

--- pywemo/ouimeaux_device/dimmer.py
+++ pywemo/ouimeaux_device/dimmer.py
@@ -1,12 +1,29 @@
 from .switch import Switch
 
-class Dimmer(Switch):		
+class Dimmer(Switch):
+    def __init__(self, *args, **kwargs):
+        Switch.__init__(self, *args, **kwargs)
+        self._brightness = {}
+
+    def get_brightness(self):
+        """
+        Get brightness from device
+        """
+        brightness = self.basicevent.GetBinaryState().get('brightness')
+        self._brightness = brightness
+
     def set_brightness(self, brightness):
         """
         Set the brightness of this device to an integer between 1-100.
+        Setting the brightness does not turn the light on, so we need
+        to check the state of the switch. 
         """
-        if self.get_state() == 0 or None:
-            self.on()
+        if brightness == 0:
+            if self.get_state() != 0:
+                self.off()
+        else:
+            if self.get_state() == 0:
+                    self.on()
 
         self.basicevent.SetBinaryState(brightness=int(brightness))
         self._brightness = int(brightness)
