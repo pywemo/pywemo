87459e1620d2642fb5da506e2bff2e2d0c507ed6
pavoni <mail@gregdowling.com>
2017-10-25 10:11:42 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='pywemo',
-      version='0.4.21',
+      version='0.4.22',
       description='Access WeMo switches using their SOAP API',
       url='http://github.com/pavoni/pywemo',
       author='Greg Dowling',
