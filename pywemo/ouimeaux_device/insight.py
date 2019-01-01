a9613c442a456a7e05aad7f950ba6249c28dbfd6
Adam Belebczuk <adam@sqldiablo.com>
2019-01-01 12:41:02 -0500

    Flake8 fixes

--- pywemo/ouimeaux_device/insight.py
+++ pywemo/ouimeaux_device/insight.py
@@ -1,3 +1,4 @@
+"""Representation of a WeMo Insight device."""
 import logging
 from datetime import datetime
 from .switch import Switch
@@ -6,18 +7,25 @@ LOG = logging.getLogger(__name__)
 
 
 class Insight(Switch):
+    """Representation of a WeMo Insight device."""
+
     def __init__(self, *args, **kwargs):
+        """Create a WeMo Switch device."""
         Switch.__init__(self, *args, **kwargs)
         self.insight_params = {}
 
     def __repr__(self):
+        """Return a string representation of the device."""
         return '<WeMo Insight "{name}">'.format(name=self.name)
 
     def update_insight_params(self):
+        """Get and parse the device attributes."""
+        # pylint: disable=maybe-no-member
         params = self.insight.GetInsightParams().get('InsightParams')
         self.insight_params = self.parse_insight_params(params)
 
     def subscription_update(self, _type, _params):
+        """Update the device attributes due to a subscription update event."""
         LOG.debug("subscription_update %s %s", _type, _params)
         if _type == "InsightParams":
             self.insight_params = self.parse_insight_params(_params)
@@ -25,6 +33,7 @@ class Insight(Switch):
         return Switch.subscription_update(self, _type, _params)
 
     def parse_insight_params(self, params):
+        """ Parse the Insight parameters."""
         (
             state,  # 0 if off, 1 if on, 8 if on but load is off
             lastchange,
@@ -43,6 +52,7 @@ class Insight(Switch):
                 'onfor': int(onfor),
                 'ontoday': int(ontoday),
                 'ontotal': int(ontotal),
+                'timeperiod':int(timeperiod),
                 'todaymw': int(float(todaymw)),
                 'totalmw': int(float(totalmw)),
                 'currentpower': int(float(currentmw)),
@@ -56,44 +66,52 @@ class Insight(Switch):
 
     @property
     def today_kwh(self):
+        """Return the kwh used today."""
         return self.insight_params['todaymw'] * 1.6666667e-8
 
     @property
     def current_power(self):
-        """
-        Returns the current power usage in mW.
-        """
+        """Return the current power usage in mW."""
         return self.insight_params['currentpower']
 
     @property
     def threshold_power(self):
         """
-        Returns the threshold power. Above this the device is on, below it is standby.
+        Return the threshold power.
+
+        Above this the device is on, below it is standby.
         """
         return self.insight_params['powerthreshold']
 
     @property
     def today_on_time(self):
+        """Return how long the device has been on today."""
         return self.insight_params['ontoday']
 
     @property
     def on_for(self):
+        """Return how long the device has been on."""
         return self.insight_params['onfor']
 
     @property
     def last_change(self):
+        """Return the last change datetime."""
         return self.insight_params['lastchange']
 
     @property
     def today_standby_time(self):
+        """Return how long the device has been in standby today."""
         return self.insight_params['ontoday']
 
     @property
     def get_standby_state(self):
+        """Return the standby state of the device."""
         state = self.insight_params['state']
+
         if state == '0':
             return 'off'
+
         if state == '1':
             return 'on'
-        else:
-            return 'standby'
+
+        return 'standby'
--- pywemo/ouimeaux_device/switch.py
+++ pywemo/ouimeaux_device/switch.py
@@ -1,32 +1,29 @@
+"""Representation of a WeMo Switch device."""
 from . import Device
 
 
 class Switch(Device):
+    """Representation of a WeMo Switch device."""
 
     def set_state(self, state):
-        """
-        Set the state of this device to on or off.
-        """
+        """Set the state of this device to on or off."""
+        # pylint: disable=maybe-no-member
         self.basicevent.SetBinaryState(BinaryState=int(state))
         self._state = int(state)
 
     def off(self):
-        """
-        Turn this device off. If already off, will return "Error".
-        """
+        """Turn this device off. If already off, will return "Error"."""
         return self.set_state(0)
 
+    # pylint: disable=invalid-name
     def on(self):
-        """
-        Turn this device on. If already on, will return "Error".
-        """
+        """Turn this device on. If already on, will return "Error"."""
         return self.set_state(1)
 
     def toggle(self):
-        """
-        Toggle the switch's state.
-        """
+        """Toggle the switch's state."""
         return self.set_state(not self.get_state())
 
     def __repr__(self):
+        """Return a string representation of the device."""
         return '<WeMo Switch "{name}">'.format(name=self.name)
