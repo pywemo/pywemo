cccc98c331b779e8cf180aa1a4a487e88926c7b1
Adam Belebczuk <adam@sqldiablo.com>
2019-01-01 13:03:13 -0500

    Flake8 fixes

--- pywemo/ouimeaux_device/lightswitch.py
+++ pywemo/ouimeaux_device/lightswitch.py
@@ -6,5 +6,5 @@ class LightSwitch(Switch):
     """Representation of a WeMo Motion device."""
 
     def __repr__(self):
-        """String representation of the device."""
+        """Return a string representation of the device."""
         return '<WeMo LightSwitch "{name}">'.format(name=self.name)
