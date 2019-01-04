9dd2cdc68d92ee3afc0a940c5674965cf908653b
Adam Belebczuk <adam@sqldiablo.com>
2019-01-04 07:44:31 -0500

    Add pypi classifiers and publish as 0.4.38

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ CONST_DESC = 'Lightweight Python module to discover and control WeMo devices'
 
 
 setup(name='pywemo',
-      version='0.4.37',
+      version='0.4.38',
       description=CONST_DESC,
       long_description=open('README.rst').read(),
       url='http://github.com/pavoni/pywemo',
@@ -15,4 +15,10 @@ setup(name='pywemo',
       license='MIT',
       install_requires=['netifaces>=0.10.0', 'requests>=2.0', 'six>=1.10.0'],
       packages=find_packages(),
-      zip_safe=True)
+      zip_safe=True,
+      classifiers=[
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+          "Topic :: Home Automation"])
