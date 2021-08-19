74c921ebcc03ffbca2f40c7fd7ad5c208808f016
Eric Severance <esev@esev.com>
2021-08-18 17:51:41 -0700

    Add subscription updates for the maker device (#288)

--- tests/ouimeaux_device/test_maker.py
+++ tests/ouimeaux_device/test_maker.py
@@ -20,7 +20,6 @@ class Test_Maker:
         maker.off()
         assert maker.get_state(force_update=True) == 0
 
-    @pytest.mark.vcr()
     def test_maker_params(self, maker):
         expected_params = {
             'hassensor': 1,
@@ -28,6 +27,109 @@ class Test_Maker:
             'switchmode': 1,
             'switchstate': 0,
         }
+        unittest.TestCase().assertDictEqual(
+            maker.maker_params, expected_params
+        )
+        assert maker.switch_state == 0
+        assert maker.sensor_state == 1
+        assert maker.switch_mode == 1
+        assert maker.has_sensor == 1
+        assert maker.get_state() == 0
+
+    def test_maker_device_type(self, maker):
+        assert maker.device_type == "Maker"
+        assert repr(maker) == '<WeMo Maker "WeMo Device">'
+
+    def test_maker_unexpected_subscription_type(self, maker):
+        assert maker.subscription_update("", "") is False
+
+    @pytest.mark.parametrize(
+        "update,expected_params",
+        [
+            # Sensor/sensorstate
+            (
+                (
+                    '<attribute><name>Sensor</name><value>0</value>'
+                    '<prevalue>1</prevalue><ts>1627869840</ts></attribute>'
+                ),
+                {
+                    'hassensor': 1,
+                    'sensorstate': 0,
+                    'switchmode': 1,
+                    'switchstate': 0,
+                },
+            ),
+            # Switch/switchstate
+            (
+                (
+                    '<attribute><name>Switch</name><value>1</value>'
+                    '<prevalue>1</prevalue><ts>1627869840</ts></attribute>'
+                ),
+                {
+                    'hassensor': 1,
+                    'sensorstate': 1,
+                    'switchmode': 1,
+                    'switchstate': 1,
+                },
+            ),
+            # SwitchMode/switchmode
+            (
+                (
+                    '<attribute><name>SwitchMode</name><value>0</value>'
+                    '<prevalue>1</prevalue><ts>1627869840</ts></attribute>'
+                ),
+                {
+                    'hassensor': 1,
+                    'sensorstate': 1,
+                    'switchmode': 0,
+                    'switchstate': 0,
+                },
+            ),
+            # SensorPresent/hassensor
+            (
+                (
+                    '<attribute><name>SensorPresent</name><value>0</value>'
+                    '<prevalue>1</prevalue><ts>1627869840</ts></attribute>'
+                ),
+                {
+                    'hassensor': 0,
+                    'sensorstate': 1,
+                    'switchmode': 1,
+                    'switchstate': 0,
+                },
+            ),
+            # Invalid state value.
+            (
+                (
+                    '<attribute><name>SensorPresent</name><value>ABC</value>'
+                    '<prevalue>1</prevalue><ts>1627869840</ts></attribute>'
+                ),
+                {
+                    'hassensor': 1,
+                    'sensorstate': 1,
+                    'switchmode': 1,
+                    'switchstate': 0,
+                },
+            ),
+            # Unexpected State name
+            (
+                (
+                    '<attribute><name>Unexpected</name><value>ABC</value>'
+                    '<prevalue>1</prevalue><ts>1627869840</ts></attribute>'
+                ),
+                {
+                    'hassensor': 1,
+                    'sensorstate': 1,
+                    'switchmode': 1,
+                    'switchstate': 0,
+                },
+            ),
+        ],
+    )
+    def test_subscription_update(self, update, expected_params, maker):
+        """Test that subscription updates happen as expected."""
+        updated = maker.subscription_update('attributeList', update)
+        assert updated is True
         unittest.TestCase().assertDictEqual(
             maker.maker_params, expected_params
         )
