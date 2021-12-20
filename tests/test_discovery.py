d92dc5cc466e3c6544ffc279097de9454299deb8
Eric Severance <esev@esev.com>
2021-12-19 19:23:32 -0800

    Only log discovery issues once per uuid/udn (#308)
    * Only log discovery issues once per uuid/udn
    
    * Add test for test_call_once_per_uuid
    
    * Test InvalidSchemaError too

--- tests/test_discovery.py
+++ tests/test_discovery.py
@@ -104,6 +104,16 @@ def test_device_from_uuid_and_location_returns_none():
         )
 
     with mock.patch(
+        "pywemo.discovery.Switch", side_effect=exceptions.InvalidSchemaError
+    ):
+        assert (
+            discovery.device_from_uuid_and_location(
+                "uuid:Socket-1_0-SERIALNUMBER", "http://127.0.0.1/setup.xml"
+            )
+            is None
+        )
+
+    with mock.patch(
         "pywemo.discovery.UnsupportedDevice",
         side_effect=exceptions.HTTPException,
     ):
@@ -130,3 +140,28 @@ def test_device_from_uuid_and_location_returns_unsupported():
             )
             == unsupported
         )
+
+
+def test_call_once_per_uuid():
+    call_count = 0
+
+    def increment_call_count():
+        nonlocal call_count
+        call_count += 1
+
+    def decrement_call_count():
+        nonlocal call_count
+        call_count -= 1
+
+    for uuid in ("a_uuid", "b_uuid"):
+        discovery._call_once_per_uuid(uuid, increment_call_count)
+        assert call_count == 1
+
+        discovery._call_once_per_uuid(uuid, decrement_call_count)
+        assert call_count == 0
+
+        discovery._call_once_per_uuid(uuid, increment_call_count)
+        assert call_count == 0
+
+        discovery._call_once_per_uuid(uuid, decrement_call_count)
+        assert call_count == 0
