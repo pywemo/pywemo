eb70798989ae7471c333852b33eb04b11665a6f3
Eric Severance <esev@esev.com>
2023-05-19 17:19:42 -0700

    Use pre-commit for code checks in build.sh (#375)
    * Use pre-commit for code checks in build.sh
    
    * Archive any changes made during build
    
    * Point to the step that failed
    
    * Use always() and failure() to run steps after a failure
    
    * Diff from the GITHUB_SHA
    
    * Remove custom "exclude" for flake8 after #376

--- scripts/build.sh
+++ scripts/build.sh
@@ -15,36 +15,8 @@ echo "===Installing pre-commit hooks==="
 pre-commit install
 
 echo
-echo "===Validate with rstcheck==="
-rstcheck README.rst
-
-echo
-echo "===Sort imports with isort==="
-ISORT_ARGS=""
-if [[ ! -z "${CI:-}" ]]; then
-  ISORT_ARGS="--check-only"
-fi
-isort $ISORT_ARGS .
-
-echo
-echo "===Format with black==="
-BLACK_ARGS=""
-if [[ ! -z "${CI:-}" ]]; then
-  BLACK_ARGS="--check"
-fi
-black $BLACK_ARGS .
-
-echo
-echo "===Lint with flake8==="
-flake8
-
-echo
-echo "===Lint with pylint==="
-pylint pywemo scripts
-
-echo
-echo "===Lint with mypy==="
-mypy .
+echo "===Running pre-commit checks==="
+pre-commit run --all-files
 
 echo
 echo "===Test with pytest and coverage==="
