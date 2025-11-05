f6ac06cea420e18bee4692268c4ed1b17e1ba6a1
Spectre5 <31610422+Spectre5@users.noreply.github.com>
2025-11-04 20:39:07 -0800

    fix clusterfuzzlite (#768)
    * fix clusterfuzzlite
    
    * Add back in some apt-get installations
    
    * try moving lxml out of instrument_imports
    
    * undo last change
    
    * Remove LD_PRELOAD
    
    * scorecard fix
    
    * Try no requirements.txt
    
    * try2
    
    * try3
    
    * Add versions, but no requirements file

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -3,23 +3,17 @@
 cd "$SRC/pywemo"
 source "./scripts/common.sh"
 enterVenv
-# Upgrade pip first.
-pip install \
-  --require-hashes \
-  --no-deps \
-  --only-binary :all: \
-  -c .clusterfuzzlite/requirements.txt \
-  pip
-# Install fuzzer dependencies.
-pip install \
-  --require-hashes \
-  --no-deps \
-  --only-binary :all: \
-  -r ./.clusterfuzzlite/requirements.txt
-# Install pyWeMo dependencies.
+
+# upgrade pip
+pip install --upgrade pip
+
+# install fuzzer dependencies
+pip install pyinstaller==6.16.0 atheris==2.3.0
+
+# install pyWeMo dependencies
 poetryInstall
 
-# Use pyinstaller to build fuzzers.
+# use pyinstaller to build fuzzers
 for fuzzer in $(find . -name '*_fuzz.py'); do
   fuzzer_basename=$(basename -s .py $fuzzer)
   fuzzer_package=${fuzzer_basename}.pkg
@@ -29,7 +23,6 @@ for fuzzer in $(find . -name '*_fuzz.py'); do
   echo "#!/bin/sh
 # LLVMFuzzerTestOneInput for fuzzer detection.
 this_dir=\$(dirname \"\$0\")
-LD_PRELOAD=\$this_dir/sanitizer_with_fuzzer.so \
 ASAN_OPTIONS=\$ASAN_OPTIONS:symbolize=1:external_symbolizer_path=\$this_dir/llvm-symbolizer:detect_leaks=0 \
 \$this_dir/$fuzzer_package \$@" > $OUT/$fuzzer_basename
   chmod +x $OUT/$fuzzer_basename
